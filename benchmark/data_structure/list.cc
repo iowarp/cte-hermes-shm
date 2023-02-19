55b80e22e9bfed97a49bc2a6d587bf52db473246
lukemartinlogan <lukemartinlogan@gmail.com>
2023-02-18 18:16:51 -0600

    Liscensing update

--- benchmark/data_structure/list.cc
+++ benchmark/data_structure/list.cc
@@ -1,3 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /*
  * Copyright (C) 2022  SCS Lab <scslab@iit.edu>,
  * Luke Logan <llogan@hawk.iit.edu>,
@@ -22,7 +34,3 @@
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
-//
-// Created by lukemartinlogan on 12/11/22.
-//
--- scripts/preamble.py
+++ scripts/preamble.py
@@ -2,13 +2,42 @@
 Prepends the preabmle to all files in the repo
 
 USAGE:
-    python3 scripts/preamble.py
+    python3 scripts/preamble.py ${HERMES_ROOT}
 """
 
 import sys,os,re
 import pathlib
 
-preamble = """/*
+hermes_preamble = """
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+""".strip()
+
+preamble2 = """
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Distributed under BSD 3-Clause license.                                   *
+* Copyright by The HDF Group.                                               *
+* Copyright by the Illinois Institute of Technology.                        *
+* All rights reserved.                                                      *
+*                                                                           *
+* This file is part of Hermes. The full Hermes copyright notice, including  *
+* terms governing use, modification, and redistribution, is contained in    *
+* the COPYING file, which can be found at the top directory. If you do not  *
+* have access to the file, you may request a copy from help@hdfgroup.org.   *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+""".strip()
+
+labstor_preamble = """
+/*
  * Copyright (C) 2022  SCS Lab <scslab@iit.edu>,
  * Luke Logan <llogan@hawk.iit.edu>,
  * Jaime Cernuda Garcia <jcernudagarcia@hawk.iit.edu>
@@ -16,9 +45,9 @@ preamble = """/*
  * Anthony Kougkas <akougkas@iit.edu>,
  * Xian-He Sun <sun@iit.edu>
  *
- * This file is part of LabStor
+ * This file is part of HermesShm
  *
- * LabStor is free software: you can redistribute it and/or modify
+ * HermesShm is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -32,18 +61,18 @@ preamble = """/*
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
-"""
+""".strip()
 
 def PrependPreamble(path):
     text = ""
     with open(path, 'r') as fp:
         text = fp.read()
-        if "Copyright (C) 2022  SCS Lab <scslab@iit.edu>" in text:
-            text = text.replace(preamble, "")
-        text = text.strip()
+        text = text.replace(hermes_preamble, "")
+        text = text.replace(preamble2, "")
+        text = text.replace(labstor_preamble, "")
         text = re.sub("//\n// Created by [^\n]*\n//\n", "", text)
-        text = preamble + text + "\n"
+        text = text.strip()
+        text = hermes_preamble + "\n\n" + text + "\n"
 
     with open(path, 'w') as fp:
         fp.write(text)
@@ -60,5 +89,5 @@ def LocateCppFiles(root):
                 PrependPreamble(full_path)
 
 
-root=pathlib.Path(__file__).parent.parent.resolve()
-LocateCppFiles(root)
+PROJECT_ROOT=pathlib.Path(__file__).parent.parent.resolve()
+LocateCppFiles(PROJECT_ROOT)
