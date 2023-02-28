675f24f3192fae6b946439c890a5f14e682e164e
Luke <llogan@hawk.iit.edu>
2023-02-28 14:10:38 -0600

    Spack repo add in ci

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -32,6 +32,7 @@ set -x
 cp scripts/ci/packages.yaml ${SPACK_DIR}/etc/spack/packages.yaml
 
 # Install hermes_shm (needed for dependencies)
+spack repo add scripts/hermes_shm
 spack install hermes_shm
 
 popd
