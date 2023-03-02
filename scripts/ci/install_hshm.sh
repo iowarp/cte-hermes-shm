9ec191a10116fe15cebbfe3d1cbe8cea164775e5
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-02 06:08:45 -0600

    Set spack env in both scripts

--- scripts/ci/install_hshm.sh
+++ scripts/ci/install_hshm.sh
@@ -3,21 +3,19 @@
 # CD into git workspace
 cd ${GITHUB_WORKSPACE}
 
-# Set spack env
-set +x
-SPACK_DIR=${INSTALL_DIR}/spack
-. ${SPACK_DIR}/share/spack/setup-env.sh
-set -x
-
 set -x
 set -e
 set -o pipefail
+
+# Set spack env
+INSTALL_DIR="${HOME}/${LOCAL}"
+SPACK_DIR=${INSTALL_DIR}/spack
+. ${SPACK_DIR}/share/spack/setup-env.sh
 
 mkdir -p "${HOME}/install"
 mkdir build
 cd build
 # export CXXFLAGS="${CXXFLAGS} -std=c++17 -Werror -Wall -Wextra"
-source ${HOME}/.bashrc
 spack load --only dependencies hermes_shm
 cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$[HOME}/install
 cmake --build . -- -j4
