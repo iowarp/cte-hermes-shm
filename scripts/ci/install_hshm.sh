0ffa050e19cb081597e755d91a838cae2d46bc97
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-02 06:57:38 -0600

    Fix accidental bracket in /home/lukemartinlogan

--- scripts/ci/install_hshm.sh
+++ scripts/ci/install_hshm.sh
@@ -17,6 +17,6 @@ mkdir build
 cd build
 # export CXXFLAGS="${CXXFLAGS} -std=c++17 -Werror -Wall -Wextra"
 spack load --only dependencies hermes_shm
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$[HOME}/install
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${HOME}/install
 cmake --build . -- -j4
 ctest -VV
