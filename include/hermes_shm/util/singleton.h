d71f66462386774e59049a92c4a6f7774c2d0a1f
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-03 15:46:12 -0600

    Different singleton types

--- include/hermes_shm/util/singleton.h
+++ include/hermes_shm/util/singleton.h
@@ -18,16 +18,93 @@
 
 namespace scs {
 
+/**
+ * Makes a singleton. Constructs the first time GetInstance is called.
+ * Requires user to define the static storage of obj_ in separate file.
+ * @tparam T
+ */
 template<typename T>
 class Singleton {
  private:
-  static T obj_;
+  static std::unique_ptr<T> obj_;
+  static hermes_shm::Mutex lock_;
+
  public:
   Singleton() = default;
+
+  /** Get or create an instance of type T */
+  inline static T* GetInstance() {
+    if (!obj_) {
+      hermes_shm::ScopedMutex lock(lock_);
+      if (obj_ == nullptr) {
+        obj_ = std::make_unique<T>();
+      }
+    }
+    return obj_.get();
+  }
+};
+
+/**
+ * Makes a singleton. Constructs during initialization of program.
+ * Requires user to define the static storage of obj_ in separate file.
+ * */
+template<typename T>
+class GlobalSingleton {
+ private:
+  static T obj_;
+ public:
+  GlobalSingleton() = default;
   static T* GetInstance() {
     return &obj_;
   }
 };
+
+/**
+ * A class to represent singleton pattern
+ * Does not require specific initialization of the static variable
+ * */
+template<typename T>
+class EasySingleton {
+ protected:
+  /** static instance. */
+  static std::unique_ptr<T> obj_;
+  static hermes_shm::Mutex lock_;
+
+ public:
+  /**
+   * Uses unique pointer to build a static global instance of variable.
+   * @tparam T
+   * @return instance of T
+   */
+  static T* GetInstance() {
+    if (obj_ == nullptr) {
+      hermes_shm::ScopedMutex lock(lock_);
+      if (obj_ == nullptr) {
+        obj_ = std::make_unique<T>();
+      }
+    }
+    return obj_.get();
+  }
+};
+template <typename T>
+std::unique_ptr<T> EasySingleton<T>::obj_ = nullptr;
+
+/**
+ * Makes a singleton. Constructs during initialization of program.
+ * Does not require specific initialization of the static variable.
+ * */
+template<typename T>
+class EasyGlobalSingleton {
+ private:
+  static T obj_;
+ public:
+  EasyGlobalSingleton() = default;
+  static T* GetInstance() {
+    return &obj_;
+  }
+};
+template <typename T>
+T EasyGlobalSingleton<T>::obj_;
 
 }  // namespace scs
 
