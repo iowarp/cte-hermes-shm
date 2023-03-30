4ebb3ecf5f9c9f5d9ab0ea6357b5fd265547276c
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-30 09:33:43 -0500

    Fix unused constants for thallium

--- benchmark/data_structure/atomic.cc
+++ benchmark/data_structure/atomic.cc
@@ -167,7 +167,7 @@ class AtomicInstructionTestSuite {
     if (test_count == 0) {
       TestOutputHeader();
     }
-    int nthreads = omp_get_num_threads();
+    size_t nthreads = omp_get_num_threads();
     ss << test_name << ","
        << atomic_type_ << ","
        << sizeof(T) << ","
