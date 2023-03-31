565f0791cf0a60f5f59fc40c20fd64f870362abd
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-31 11:14:28 -0500

    Fix lint issues

--- include/hermes_shm/util/logging.h
+++ include/hermes_shm/util/logging.h
@@ -1,6 +1,14 @@
-//
-// Created by lukemartinlogan on 3/31/23.
-//
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_LOGGING_H_
 #define HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_LOGGING_H_
@@ -129,4 +137,4 @@ class Logger {
 
 }  // namespace hshm
 
-#endif //HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_LOGGING_H_
+#endif  // HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_LOGGING_H_
