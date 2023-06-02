7d85ae60faed3ecf1df73193ce444e4727644f75
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-02 16:19:31 -0500

    Normalizes spaces a bit for coverage file

--- scripts/ci/coverage.sh
+++ scripts/ci/coverage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-COVERAGE_DIR=${GITHUB_WORKSPACE}/coverage
-mkdir -p ${COVERAGE_DIR}
-cd ${GITHUB_WORKSPACE}/build
+COVERAGE_DIR="${GITHUB_WORKSPACE}/coverage"
+mkdir -p "${COVERAGE_DIR}"
+cd "${GITHUB_WORKSPACE}/build"
 lcov -c -d . -o "${COVERAGE_DIR}/tmp.info"
-lcov --remove "${COVERAGE_DIR}/tmp.info"   \
-              "/usr/include/*"             \
-              "${HOME}/*"         \
+lcov --remove "${COVERAGE_DIR}/tmp.info" \
+              "/usr/include/*" \
+              "${HOME}/*" \
               "../" \
-              -o ${COVERAGE_DIR}/lcov.info
+              -o "${COVERAGE_DIR}/lcov.info"
