4f2d6d0295f2ffa3ed582ca3b7530879a1ebb56c
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-02 04:50:24 -0500

    Disable coverage

---
+++ scripts/ci/coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+COVERAGE_DIR=${GITHUB_WORKSPACE}/coverage
+mkdir -p ${COVERAGE_DIR}
+cd ${GITHUB_WORKSPACE}/build
+lcov -c -d . -o "${COVERAGE_DIR}/tmp.info"
+lcov --remove "${COVERAGE_DIR}/tmp.info"   \
+              "/usr/include/*"             \
+              "${HOME}/*"         \
+              "*/stb_ds.h"                 \
+              -o ${COVERAGE_DIR}/lcov.info
