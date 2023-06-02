b1dbe263809d4c8fce700f08750eb8fabedbd96e
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-02 04:26:58 -0500

    Test locking implementation performance

--- benchmark/data_structure/queue.cc
+++ benchmark/data_structure/queue.cc
@@ -63,11 +63,10 @@ class QueueTest {
   }
 
   /** Run the tests */
-  void Test() {
-    size_t count = 10000;
+  void Test(size_t count_per_rank=100000, int nthreads = 1) {
     // AllocateTest(count);
-    EmplaceTest(count);
-    DequeueTest(count);
+    EmplaceTest(count_per_rank, nthreads);
+    DequeueTest(count_per_rank, nthreads);
   }
 
   /**====================================
@@ -88,32 +87,33 @@ class QueueTest {
   }
 
   /** Emplace after reserving enough space */
-  void EmplaceTest(size_t count) {
+  void EmplaceTest(size_t count_per_rank, int nthreads) {
     Timer t;
-    StringOrInt<T> var(124);
 
+    size_t count = count_per_rank * nthreads;
     Allocate(count);
     t.Resume();
-    Emplace(count);
+    Emplace(count_per_rank, nthreads);
     t.Pause();
 
-    TestOutput("Enqueue", t, count);
+    TestOutput("Enqueue", t, count, nthreads);
     Destroy();
   }
 
   /** Emplace after reserving enough space */
-  void DequeueTest(size_t count) {
+  void DequeueTest(size_t count_per_rank, int nthreads) {
     Timer t;
     StringOrInt<T> var(124);
 
+    size_t count = count_per_rank * nthreads;
     Allocate(count);
-    Emplace(count);
+    Emplace(count, 1);
 
     t.Resume();
-    Dequeue(count);
+    Dequeue(count_per_rank, nthreads);
     t.Pause();
 
-    TestOutput("Dequeue", t, count);
+    TestOutput("Dequeue", t, count, nthreads);
     Destroy();
   }
 
@@ -123,47 +123,56 @@ class QueueTest {
    * ===================================*/
 
   /** Output as CSV */
-  void TestOutput(const std::string &test_name, Timer &t, size_t count) {
-    HIPRINT("{},{},{},{},{}\n",
-            test_name, queue_type_, internal_type_, t.GetMsec(),
+  void TestOutput(const std::string &test_name, Timer &t,
+                  size_t count, int nthreads) {
+    HIPRINT("{},{},{},{},{},{}\n",
+            test_name, queue_type_, internal_type_, nthreads, t.GetMsec(),
             (float)count / t.GetUsec())
   }
 
-  /** Get element at position i */
-  void Dequeue(size_t count) {
-    for (size_t i = 0; i < count; ++i) {
-      if constexpr(std::is_same_v<QueueT, std::queue<T>>) {
-        lock_.lock();
-        T &x = queue_->front();
-        USE(x);
-        queue_->pop();
-        lock_.unlock();
-      } else if constexpr(std::is_same_v<QueueT, hipc::mpsc_queue<T>>) {
-        queue_->pop(*x_);
-        USE(*x_);
-      } else if constexpr(std::is_same_v<QueueT, hipc::spsc_queue<T>>) {
-        queue_->pop(*x_);
-        USE(*x_);
-      } else if constexpr(std::is_same_v<QueueT, hipc::ticket_queue<T>>) {
-        while (queue_->pop(*x_).IsNull());
+  /** Emplace elements into the queue */
+  void Emplace(size_t count_per_rank, int nthreads) {
+    omp_set_dynamic(0);
+#pragma omp parallel num_threads(nthreads)
+    {
+      StringOrInt<T> var(124);
+      for (size_t i = 0; i < count_per_rank; ++i) {
+        if constexpr(std::is_same_v<QueueT, std::queue<T>>) {
+          lock_.lock();
+          queue_->emplace(var.Get());
+          lock_.unlock();
+        } else if constexpr(std::is_same_v<QueueT, hipc::mpsc_queue<T>>) {
+          queue_->emplace(var.Get());
+        } else if constexpr(std::is_same_v<QueueT, hipc::spsc_queue<T>>) {
+          queue_->emplace(var.Get());
+        } else if constexpr(std::is_same_v<QueueT, hipc::ticket_queue<T>>) {
+          queue_->emplace(var.Get());
+        }
       }
     }
   }
 
-  /** Emplace elements into the queue */
-  void Emplace(size_t count) {
-    StringOrInt<T> var(124);
-    for (size_t i = 0; i < count; ++i) {
-      if constexpr(std::is_same_v<QueueT, std::queue<T>>) {
-        lock_.lock();
-        queue_->emplace(var.Get());
-        lock_.unlock();
-      } else if constexpr(std::is_same_v<QueueT, hipc::mpsc_queue<T>>) {
-        queue_->emplace(var.Get());
-      } else if constexpr(std::is_same_v<QueueT, hipc::spsc_queue<T>>) {
-        queue_->emplace(var.Get());
-      } else if constexpr(std::is_same_v<QueueT, hipc::ticket_queue<T>>) {
-        queue_->emplace(var.Get());
+  /** Get element at position i */
+  void Dequeue(size_t count_per_rank, int nthreads) {
+    omp_set_dynamic(0);
+#pragma omp parallel num_threads(nthreads)
+    {
+      for (size_t i = 0; i < count_per_rank; ++i) {
+        if constexpr(std::is_same_v<QueueT, std::queue<T>>) {
+          lock_.lock();
+          T &x = queue_->front();
+          USE(x);
+          queue_->pop();
+          lock_.unlock();
+        } else if constexpr(std::is_same_v<QueueT, hipc::mpsc_queue<T>>) {
+          queue_->pop(*x_);
+          USE(*x_);
+        } else if constexpr(std::is_same_v<QueueT, hipc::spsc_queue<T>>) {
+          queue_->pop(*x_);
+          USE(*x_);
+        } else if constexpr(std::is_same_v<QueueT, hipc::ticket_queue<T>>) {
+          while (queue_->pop(*x_).IsNull());
+        }
       }
     }
   }
@@ -200,24 +209,31 @@ class QueueTest {
 };
 
 void FullQueueTest() {
+  const size_t count_per_rank = 100000;
   // std::queue tests
-  QueueTest<size_t, std::queue<size_t>>().Test();
+  QueueTest<size_t, std::queue<size_t>>().Test(count_per_rank, 1);
+  QueueTest<size_t, std::queue<size_t>>().Test(count_per_rank, 4);
+  QueueTest<size_t, std::queue<size_t>>().Test(count_per_rank, 8);
+  QueueTest<size_t, std::queue<size_t>>().Test(count_per_rank, 16);
   // QueueTest<std::string, std::queue<std::string>>().Test();
 
   // hipc::ticket_queue tests
-  QueueTest<size_t, hipc::mpsc_queue<size_t>>().Test();
+  QueueTest<size_t, hipc::ticket_queue<size_t>>().Test(count_per_rank, 1);
+  QueueTest<size_t, hipc::ticket_queue<size_t>>().Test(count_per_rank, 4);
+  QueueTest<size_t, hipc::ticket_queue<size_t>>().Test(count_per_rank, 8);
+  QueueTest<size_t, hipc::ticket_queue<size_t>>().Test(count_per_rank, 16);
+  // QueueTest<std::string, hipc::mpsc_queue<std::string>>().Test();
+  // QueueTest<hipc::string, hipc::mpsc_queue<hipc::string>>().Test();
+
+  // hipc::mpsc_queue tests
+  QueueTest<size_t, hipc::mpsc_queue<size_t>>().Test(count_per_rank, 1);
   // QueueTest<std::string, hipc::mpsc_queue<std::string>>().Test();
   // QueueTest<hipc::string, hipc::mpsc_queue<hipc::string>>().Test();
 
   // hipc::spsc_queue tests
-  QueueTest<size_t, hipc::spsc_queue<size_t>>().Test();
+  QueueTest<size_t, hipc::spsc_queue<size_t>>().Test(count_per_rank, 1);
   // QueueTest<std::string, hipc::spsc_queue<std::string>>().Test();
   // QueueTest<hipc::string, hipc::spsc_queue<hipc::string>>().Test();
-
-  // hipc::ticket_queue tests
-  QueueTest<size_t, hipc::ticket_queue<size_t>>().Test();
-  // QueueTest<std::string, hipc::mpsc_queue<std::string>>().Test();
-  // QueueTest<hipc::string, hipc::mpsc_queue<hipc::string>>().Test();
 
 }
 
