fddbf82dd85f3d93da53f55c2601aeaaa1be1623
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-03 02:43:12 -0500

    Debug info for coverage directory

--- scripts/ci/coverage.sh
+++ scripts/ci/coverage.sh
@@ -7,7 +7,12 @@ lcov --remove "${COVERAGE_DIR}/tmp.info" \
               "/usr/*" \
               "*/spack/*" \
               -o "${COVERAGE_DIR}/lcov.info"
-genhtml "${COVERAGE_DIR}/tmp.info" --output-directory coverage_report
+genhtml "${COVERAGE_DIR}/lcov.info" --output-directory coverage_report
+
+echo "Placed coverage info in ${COVERAGE_DIR}/lcov.info"
+cd ${COVERAGE_DIR}
+`pwd`
+ls
 
 #lcov -c -d . -o "tmp.info"
 #lcov --remove "tmp.info" \
