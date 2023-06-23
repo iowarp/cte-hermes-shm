bf8b56c41a6e2848c46e08d597346eb90cc02e05
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-22 22:03:53 -0500

    type simplification for numbers

---
+++ include/hermes_shm/types/numbers.h
@@ -0,0 +1,25 @@
+//
+// Created by lukemartinlogan on 6/22/23.
+//
+
+#ifndef HERMES_SHM_INCLUDE_HERMES_SHM_TYPES_NUMBERS_H_
+#define HERMES_SHM_INCLUDE_HERMES_SHM_TYPES_NUMBERS_H_
+
+#include <cstdint>
+
+namespace hshm {
+
+typedef uint8_t u8;   /**< 8-bit unsigned integer */
+typedef uint16_t u16; /**< 16-bit unsigned integer */
+typedef uint32_t u32; /**< 32-bit unsigned integer */
+typedef uint64_t u64; /**< 64-bit unsigned integer */
+typedef int8_t i8;    /**< 8-bit signed integer */
+typedef int16_t i16;  /**< 16-bit signed integer */
+typedef int32_t i32;  /**< 32-bit signed integer */
+typedef int64_t i64;  /**< 64-bit signed integer */
+typedef float f32;    /**< 32-bit float */
+typedef double f64;   /**< 64-bit float */
+
+}  // namespace hshm
+
+#endif //HERMES_SHM_INCLUDE_HERMES_SHM_TYPES_NUMBERS_H_
