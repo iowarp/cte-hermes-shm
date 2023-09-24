006f99cafbb13652198a515349a948657e34a93e
lukemartinlogan <lukemartinlogan@gmail.com>
2023-09-24 01:32:41 -0500

    Add fiber

--- scripts/hermes_shm/packages/hermes_shm/package.py
+++ scripts/hermes_shm/packages/hermes_shm/package.py
@@ -8,7 +8,7 @@ class HermesShm(CMakePackage):
     depends_on('catch2@3.0.1')
     # depends_on('mpi')
     depends_on('mpich@3.3.2')
-    depends_on('boost@1.7: +context')
+    depends_on('boost@1.7: +context +fiber')
     depends_on('cereal')
     depends_on('yaml-cpp')
     depends_on('doxygen@1.9.3')
