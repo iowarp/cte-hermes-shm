30d1d46b6687668500e5ef1b0681256a1e5e66f4
lukemartinlogan <lukemartinlogan@gmail.com>
2024-02-07 21:34:24 -0600

    Update hermes_shm cmake

--- scripts/hermes_shm/packages/hermes_shm/package.py
+++ scripts/hermes_shm/packages/hermes_shm/package.py
@@ -4,18 +4,41 @@ class HermesShm(CMakePackage):
     homepage = "https://github.com/lukemartinlogan/hermes_shm/wiki"
     git = "https://github.com/lukemartinlogan/hermes_shm.git"
     version('master', branch='master')
-    depends_on('mochi-thallium~cereal@0.10.1')
+    version("1.1.0", sha256="080d5361cff22794b670e4544c532926ca8b6d6ec695af25596efe035bfffea5")
+    version("1.0.0", sha256="a79f01d531ce89985ad59a2f62b41d74c2385e48d929e2f4ad895ae34137573b")
+
+    # Main variants
+    variant('debug', default=False, description='Build shared libraries')
+    variant('mochi', default=True, description='Build with mochi-thallium support')
+    variant('cereal', default=True, description='Build with cereal support')
+    variant('boost', default=True, description='Build with boost support')
+    variant('mpiio', default=True, description='Build with MPI support')
+    variant('vfd', default=False, description='Build with HDF5 support')
+    variant('zmq', default=False, description='Build ZeroMQ tests')
+    variant('adios', default=False, description='Build Adios support')
+
+    # Required deps
     depends_on('catch2@3.0.1')
-    # depends_on('mpi')
-    depends_on('mpich@3.3.2')
-    depends_on('boost@1.7: +context +fiber')
-    depends_on('cereal')
     depends_on('yaml-cpp')
     depends_on('doxygen@1.9.3')
 
-    variant('debug', default=False, description='Build shared libraries')
+    # Machine variants
+    variant('ares', default=False, description='Build in ares')
+    depends_on('libfabric fabrics=sockets,tcp,udp,verbs,mlx,rxm,rxd,shm',
+               when='+ares')
 
-    variant('compress', default=False, description='Build with compression support')
+    # Main dependencies
+    depends_on('mochi-thallium~cereal@0.10.1', when='+mochi')
+    depends_on('cereal', when='+cereal')
+    depends_on('boost@1.7: +context +fiber', when='+boost')
+    depends_on('mpi', when='+mpiio')
+    depends_on('hdf5@1.14.0', when='+vfd')
+    depends_on('libzmq', '+zmq')
+    depends_on('adios2', when='+adios')
+
+    # Compress variant
+    variant('compress', default=False,
+            description='Build with compression support')
     depends_on('lzo', when='+compress')
     depends_on('bzip2', when='+compress')
     depends_on('zstd', when='+compress')
@@ -26,8 +49,10 @@ class HermesShm(CMakePackage):
     depends_on('snappy', when='+compress')
     depends_on('c-blosc2', when='+compress')
 
-    variant('encrypt', default=False, description='Build with encryption support')
-    depends_on('cryptopp', when='+encrypt')
+    # Encryption variant
+    variant('encrypt', default=False,
+            description='Build with encryption support')
+    depends_on('openssl', when='+encrypt')
 
     def cmake_args(self):
         args = []
@@ -52,6 +77,7 @@ class HermesShm(CMakePackage):
         self.set_lib(env, '{}/lib'.format(self.prefix))
         self.set_lib(env, '{}/lib64'.format(self.prefix))
         env.prepend_path('CMAKE_PREFIX_PATH', '{}/cmake'.format(self.prefix))
+        env.prepend_path('CMAKE_MODULE_PATH', '{}/cmake'.format(self.prefix))
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         self.set_flags(spack_env)
