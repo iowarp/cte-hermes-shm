614ed78ebef672fca7ff814c3dac6705e60af69b
lukemartinlogan <lukemartinlogan@gmail.com>
2024-04-11 01:34:12 -0500

    Wrap around mpsc ipc queue

--- include/hermes_shm/data_structures/containers/mpsc_queue.h
+++ include/hermes_shm/data_structures/containers/mpsc_queue.h
@@ -15,6 +15,7 @@
 
 #include <vector>
 #include "hermes_shm/types/qtok.h"
+#include "hermes_shm/ipc/mpsc_queue.h"
 
 namespace hshm {
 
@@ -37,10 +38,7 @@ class mpsc_queue;
 template<typename T>
 class mpsc_queue {
  public:
-  std::vector<std::pair<bitfield32_t, T>> queue_;
-  std::atomic<_qtok_t> tail_;
-  std::atomic<_qtok_t> head_;
-  bitfield32_t flags_;
+  hipc::uptr<hipc::mpsc_queue<T>> queue_;
 
  public:
   /**====================================
@@ -49,9 +47,7 @@ class mpsc_queue {
 
   /** SHM constructor. Default. */
   explicit mpsc_queue(size_t depth = 1024) {
-    queue_.resize(depth);
-    flags_.Clear();
-    SetNull();
+    queue_ = hipc::make_uptr<hipc::mpsc_queue<T>>(depth);
   }
 
   /**====================================
@@ -60,16 +56,12 @@ class mpsc_queue {
 
   /** SHM copy constructor */
   explicit mpsc_queue(const mpsc_queue &other) {
-    head_ = other.head_.load();
-    tail_ = other.tail_.load();
     queue_ = other.queue_;
   }
 
   /** SHM copy assignment operator */
   mpsc_queue& operator=(const mpsc_queue &other) {
     if (this != &other) {
-      head_ = other.head_.load();
-      tail_ = other.tail_.load();
       queue_ = other.queue_;
     }
     return *this;
@@ -81,8 +73,6 @@ class mpsc_queue {
 
   /** SHM move constructor. */
   mpsc_queue(mpsc_queue &&other) noexcept {
-    head_ = other.head_.load();
-    tail_ = other.tail_.load();
     queue_ = std::move(other.queue_);
     other.SetNull();
   }
@@ -90,8 +80,6 @@ class mpsc_queue {
   /** SHM move assignment operator. */
   mpsc_queue& operator=(mpsc_queue &&other) noexcept {
     if (this != &other) {
-      head_ = other.head_.load();
-      tail_ = other.tail_.load();
       queue_ = std::move(other.queue_);
       other.SetNull();
     }
@@ -104,13 +92,12 @@ class mpsc_queue {
 
   /** Check if the list is empty */
   bool IsNull() const {
-    return queue_.IsNull();
+    return queue_->IsNull();
   }
 
   /** Sets this list as empty */
   void SetNull() {
-    head_ = 0;
-    tail_ = 0;
+    queue_->SetNull();
   }
 
   /**====================================
@@ -120,129 +107,33 @@ class mpsc_queue {
   /** Construct an element at \a pos position in the list */
   template<typename ...Args>
   qtok_t emplace(Args&&... args) {
-    // Allocate a slot in the queue
-    // The slot is marked NULL, so pop won't do anything if context switch
-    _qtok_t head = head_.load();
-    _qtok_t tail = tail_.fetch_add(1);
-    size_t size = tail - head + 1;
-
-    // Check if there's space in the queue.
-    if (size > queue_.size()) {
-      while (true) {
-        head = head_.load();
-        size = tail - head + 1;
-        if (size <= queue_.size()) {
-          break;
-        }
-        HERMES_THREAD_MODEL->Yield();
-      }
-    }
-
-    // Emplace into queue at our slot
-    uint32_t idx = tail % queue_.size();
-    auto iter = queue_.begin() + idx;
-    hipc::Allocator::DestructObj(*iter);
-    hipc::Allocator::ConstructObj(
-        iter->second, std::forward<Args>(args)...);
-
-    // Let pop know that the data is fully prepared
-    std::pair<bitfield32_t, T> &entry = (*iter);
-    entry.first.SetBits(1);
-    return qtok_t(tail);
+    return queue_->emplace(std::forward<Args>(args)...);
   }
 
  public:
   /** Consumer pops the head object */
   qtok_t pop(T &val) {
-    // Don't pop if there's no entries
-    _qtok_t head = head_.load();
-    _qtok_t tail = tail_.load();
-    if (head >= tail) {
-      return qtok_t::GetNull();
-    }
-
-    // Pop the element, but only if it's marked valid
-    _qtok_t idx = head % queue_.size();
-    std::pair<bitfield32_t, T> &entry = queue_[idx];
-    if (entry.first.Any(1)) {
-      val = std::move(entry.second);
-      entry.first.Clear();
-      head_.fetch_add(1);
-      return qtok_t(head);
-    } else {
-      return qtok_t::GetNull();
-    }
+    return queue_->pop(val);
   }
 
   /** Consumer pops the head object */
   qtok_t pop() {
-    // Don't pop if there's no entries
-    _qtok_t head = head_.load();
-    _qtok_t tail = tail_.load();
-    if (head >= tail) {
-      return qtok_t::GetNull();
-    }
-
-    // Pop the element, but only if it's marked valid
-    _qtok_t idx = head % queue_.size();
-    std::pair<bitfield32_t, T> &entry = queue_[idx];
-    if (entry.first.Any(1)) {
-      entry.first.Clear();
-      head_.fetch_add(1);
-      return qtok_t(head);
-    } else {
-      return qtok_t::GetNull();
-    }
+    return queue_->pop();
   }
 
   /** Consumer peeks an object */
   qtok_t peek(T *&val, int off = 0) {
-    // Don't pop if there's no entries
-    _qtok_t head = head_.load() + off;
-    _qtok_t tail = tail_.load();
-    if (head >= tail) {
-      return qtok_t::GetNull();
-    }
-
-    // Pop the element, but only if it's marked valid
-    _qtok_t idx = (head) % queue_.size();
-    std::pair<bitfield32_t, T> &entry = queue_[idx];
-    if (entry.first.Any(1)) {
-      val = &entry.second;
-      return qtok_t(head);
-    } else {
-      return qtok_t::GetNull();
-    }
+    return queue_->peek(val, off);
   }
 
   /** Consumer peeks an object */
   qtok_t peek(std::pair<bitfield32_t, T> *&val, int off = 0) {
-    // Don't pop if there's no entries
-    _qtok_t head = head_.load() + off;
-    _qtok_t tail = tail_.load();
-    if (head >= tail) {
-      return qtok_t::GetNull();
-    }
-
-    // Pop the element, but only if it's marked valid
-    _qtok_t idx = (head) % queue_.size();
-    std::pair<bitfield32_t, T> &entry = queue_[idx];
-    if (entry.first.Any(1)) {
-      val = &entry;
-      return qtok_t(head);
-    } else {
-      return qtok_t::GetNull();
-    }
+    return queue_->peek(val, off);
   }
 
   /** Get size at this moment */
   size_t GetSize() {
-    size_t tail = tail_.load();
-    size_t head = head_.load();
-    if (tail < head) {
-      return 0;
-    }
-    return tail - head;
+    return queue_->GetSize();
   }
 };
 
