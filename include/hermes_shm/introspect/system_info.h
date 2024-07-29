6072437a3324479c57e09ccb427909a3311dcd7a
lukemartinlogan <lukemartinlogan@gmail.com>
2024-07-29 02:54:44 -0500

    Add CPU rate limiting to introspect

--- include/hermes_shm/introspect/system_info.h
+++ include/hermes_shm/introspect/system_info.h
@@ -16,6 +16,9 @@
 #include <unistd.h>
 #include <sys/sysinfo.h>
 #include "hermes_shm/util/singleton/_global_singleton.h"
+#include "hermes_shm/util/formatter.h"
+#include <iostream>
+#include <fstream>
 
 #define HERMES_SYSTEM_INFO \
   hshm::GlobalSingleton<hshm::SystemInfo>::GetInstance()
@@ -30,6 +33,7 @@ struct SystemInfo {
   int uid_;
   int gid_;
   size_t ram_size_;
+  std::vector<size_t> cur_cpu_freq_;
 
   SystemInfo() {
     pid_ = getpid();
@@ -40,6 +44,46 @@ struct SystemInfo {
     uid_ = getuid();
     gid_ = getgid();
     ram_size_ = info.totalram;
+    cur_cpu_freq_.resize(ncpu_);
+    RefreshCpuFreqKhz();
+  }
+
+  size_t RefreshCpuFreqKhz() {
+    for (int i = 0; i < ncpu_; ++i) {
+      cur_cpu_freq_[i] = GetCpuFreqKhz(i);
+    }
+  }
+
+  size_t GetCpuFreqKhz(int cpu) {
+    // Read /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_cur_freq",
+        cpu);
+    std::ifstream cpu_file(cpu_str);
+    size_t freq_khz;
+    cpu_file >> freq_khz;
+    return freq_khz;
+  }
+
+  void SetCpuFreqMhz(int cpu, size_t cpu_freq_khz) {
+    SetCpuMinFreqMhz(cpu, cpu_freq_khz);
+    SetCpuMaxFreqMhz(cpu, cpu_freq_khz);
+  }
+
+  void SetCpuMinFreqMhz(int cpu, size_t cpu_freq_khz) {
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_min_freq",
+        cpu);
+    std::ofstream min_freq_file(cpu_str);
+    min_freq_file << cpu_freq_khz;
+  }
+
+  void SetCpuMaxFreqMhz(int cpu, size_t cpu_freq_khz) {
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_max_freq",
+        cpu);
+    std::ofstream max_freq_file(cpu_str);
+    max_freq_file << cpu_freq_khz;
   }
 };
 
