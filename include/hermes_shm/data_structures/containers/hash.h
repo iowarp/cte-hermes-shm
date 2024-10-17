d32b60d98e33cc97c82a846a740b4c125f470a22
Luke Logan <lukemartinlogan@gmail.com>
2024-10-17 13:57:55 -0500

    Add simple hash for numbers

--- include/hermes_shm/data_structures/containers/hash.h
+++ include/hermes_shm/data_structures/containers/hash.h
@@ -23,6 +23,44 @@ HSHM_CROSS_FUN size_t string_hash(const StringT &text) {
   return sum;
 }
 
+/** Integer hash function */
+template<typename T>
+HSHM_INLINE_CROSS_FUN static size_t number_hash(const T &val) {
+  if constexpr (sizeof(T) == 1) {
+    return static_cast<size_t>(val);
+  } else if constexpr (sizeof(T) == 2) {
+    return static_cast<size_t>(val);
+  } else if constexpr (sizeof(T) == 4) {
+    return static_cast<size_t>(val);
+  } else if constexpr (sizeof(T) == 8) {
+    return static_cast<size_t>(val);
+  } else {
+    return 0;
+  }
+}
+
+/** HSHM integer hash */
+#define HERMES_INTEGER_HASH(T) \
+template<> \
+struct hash<T> { \
+  HSHM_CROSS_FUN size_t operator()(const T &number) const { return number_hash(number); } \
+};
+
+HERMES_INTEGER_HASH(bool);
+HERMES_INTEGER_HASH(char);
+HERMES_INTEGER_HASH(short);
+HERMES_INTEGER_HASH(int);
+HERMES_INTEGER_HASH(long);
+HERMES_INTEGER_HASH(long long);
+HERMES_INTEGER_HASH(float);
+HERMES_INTEGER_HASH(double);
+
+HERMES_INTEGER_HASH(unsigned char);
+HERMES_INTEGER_HASH(unsigned short);
+HERMES_INTEGER_HASH(unsigned int);
+HERMES_INTEGER_HASH(unsigned long);
+HERMES_INTEGER_HASH(unsigned long long);
+
 }
 
 #endif //HERMES_SHM_INCLUDE_HERMES_SHM_DATA_STRUCTURES_CONTAINERS_HASH_H_
