3878a5b7225805dbb4167f2bd290a3213d387e5a
lukemartinlogan <lukemartinlogan@gmail.com>
2024-11-18 15:21:30 -0500

    Update sysinfo

--- include/hermes_shm/introspect/system_info.h
+++ include/hermes_shm/introspect/system_info.h
@@ -16,6 +16,9 @@
 #include <unistd.h>
 #include <sys/sysinfo.h>
 #include "hermes_shm/util/singleton/_singleton.h"
+#include "hermes_shm/util/formatter.h"
+#include <iostream>
+#include <fstream>
 
 #define HERMES_SYSTEM_INFO \
   hshm::LockfreeSingleton<hshm::SystemInfo>::GetInstance()
@@ -30,6 +33,9 @@ struct SystemInfo {
   int uid_;
   int gid_;
   size_t ram_size_;
+#ifdef HSHM_IS_HOST
+  std::vector<size_t> cur_cpu_freq_;
+#endif
 
   HSHM_CROSS_FUN
   void RefreshInfo() {
@@ -42,7 +48,81 @@ struct SystemInfo {
     uid_ = getuid();
     gid_ = getgid();
     ram_size_ = info.totalram;
+    cur_cpu_freq_.resize(ncpu_);
+    RefreshCpuFreqKhz();
 #endif
+  }
+
+  void RefreshCpuFreqKhz() {
+    for (int i = 0; i < ncpu_; ++i) {
+      cur_cpu_freq_[i] = GetCpuFreqKhz(i);
+    }
+  }
+
+  size_t GetCpuFreqKhz(int cpu) {
+    // Read /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_cur_freq",
+        cpu);
+    std::ifstream cpu_file(cpu_str);
+    size_t freq_khz;
+    cpu_file >> freq_khz;
+    return freq_khz;
+  }
+
+  size_t GetCpuMaxFreqKhz(int cpu) {
+    // Read /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_max_freq",
+        cpu);
+    std::ifstream cpu_file(cpu_str);
+    size_t freq_khz;
+    cpu_file >> freq_khz;
+    return freq_khz;
+  }
+
+  size_t GetCpuMinFreqKhz(int cpu) {
+    // Read /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_min_freq",
+        cpu);
+    std::ifstream cpu_file(cpu_str);
+    size_t freq_khz;
+    cpu_file >> freq_khz;
+    return freq_khz;
+  }
+
+  size_t GetCpuMinFreqMhz(int cpu) {
+    return GetCpuMinFreqKhz(cpu) / 1000;
+  }
+
+  size_t GetCpuMaxFreqMhz(int cpu) {
+    return GetCpuMaxFreqKhz(cpu) / 1000;
+  }
+
+  void SetCpuFreqMhz(int cpu, size_t cpu_freq_mhz) {
+    SetCpuFreqKhz(cpu, cpu_freq_mhz * 1000);
+  }
+
+  void SetCpuFreqKhz(int cpu, size_t cpu_freq_khz) {
+    SetCpuMinFreqKhz(cpu, cpu_freq_khz);
+    SetCpuMaxFreqKhz(cpu, cpu_freq_khz);
+  }
+
+  void SetCpuMinFreqKhz(int cpu, size_t cpu_freq_khz) {
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_min_freq",
+        cpu);
+    std::ofstream min_freq_file(cpu_str);
+    min_freq_file << cpu_freq_khz;
+  }
+
+  void SetCpuMaxFreqKhz(int cpu, size_t cpu_freq_khz) {
+    std::string cpu_str = hshm::Formatter::format(
+        "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_max_freq",
+        cpu);
+    std::ofstream max_freq_file(cpu_str);
+    max_freq_file << cpu_freq_khz;
   }
 };
 
