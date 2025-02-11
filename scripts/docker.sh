086de10cb4b424d87135cb0e4929efb5d32fa0df
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-11 02:55:32 -0600

    Add latest

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,3 +3,5 @@ VERSION=v2.0.1
 docker build -t grc-spack docker -f docker/grc-spack.Dockerfile
 docker tag grc-spack lukemartinlogan/grc-repo:${VERSION}
 docker push lukemartinlogan/grc-repo:${VERSION}
+docker tag grc-spack lukemartinlogan/grc-repo:latest
+docker push lukemartinlogan/grc-repo:latest
