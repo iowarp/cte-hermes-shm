b385b838bd322886bfc54775719817e0d024f02d
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-11 02:36:48 -0600

    Commit version

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,3 +1,4 @@
+VERSION=v2.0.1
 docker build -t grc-spack docker -f docker/grc-spack.Dockerfile
-docker tag grc-spack lukemartinlogan/grc-repo:v2.0.0
-docker push lukemartinlogan/grc-repo:v2.0.0
+docker tag grc-spack lukemartinlogan/grc-repo:${VERSION}
+docker push lukemartinlogan/grc-repo:${VERSION}
