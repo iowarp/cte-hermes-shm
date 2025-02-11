cc022d8260e996ce330bfdc4f46f2fb71a1ecaf4
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-11 07:57:06 -0600

    Fix SetNull for allocid

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,6 @@
 # docker login
 VERSION=v2.0.1
-docker build --no-cache -t grc-spack docker -f docker/grc-spack.Dockerfile
+docker build -t grc-spack docker -f docker/grc-spack.Dockerfile
 docker tag grc-spack lukemartinlogan/grc-repo:${VERSION}
 docker push lukemartinlogan/grc-repo:${VERSION}
 docker tag grc-spack lukemartinlogan/grc-repo:latest
